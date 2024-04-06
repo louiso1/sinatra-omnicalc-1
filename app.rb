b2e6921c6f995e4378201270fb81d46fa1b8113b
louiso1 <114828731+louiso1@users.noreply.github.com>
2024-04-06 22:39:08 +0000

    preliminary work

--- app.rb
+++ app.rb
@@ -2,8 +2,44 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:main_page)
+end
+
+# Square Values
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/result") do
+  @number = params.fetch("number").to_i
+  @square = @number ** 2
+  erb(:square_result)
+end
+
+# Square Root Values
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  @number = params.fetch("number").to_i
+  @square_root_val = @number ** 0.5
+  erb(:square_root_results)
+end
+
+# Payment Form
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("apr").to_i
+  @years = params.fetch("years").to_i
+  @principal = params.fetch("principal").to_i
+
+  numerator = @apr * @principal
+  denominator = 1 - ((1 + @apr) ** (-@years))
+
+  @payment = numerator / denominator
 end
